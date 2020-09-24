35ac0934bb8968fdd295943b67719fb9c5481f42
Eric Casey <ericcasey509@gmail.com>
2020-09-24 12:04:35 -0400

    readme, gitignore, logs

--- README.md
+++ README.md
@@ -1,40 +1,77 @@
 # The Headhunter.py Growth Hack
 ## By Eric Casey
 
-*Disclaimer #0: This is a work in progress (Oct 2020)*   
-*Disclaimer #1: If you're from Linkedin and want this taken down LMK*   
-*Disclaimer #2: If I viewed your profile recently I've kept track of every profile I've viewed so I don't bother anyone twice* 
+* *Disclaimer #0: This is a work in progress (Sept/Oct 2020)*   
+* *Disclaimer #1: If you're from Linkedin and want this taken down LMK.*   
+* *Disclaimer #2: If I viewed your profile recently I've kept track of every profile I've viewed so I don't bother anyone twice, the last time I did this was several years ago.* 
+
+### wtf is this?
+This is a proof-of-concept for a bot that conducts wide searches for user-provided job titles (like 'recruiter') and locales (like Lima, Peru) and automatically views each user's profile.
+
+Then, depending on that user's number of connections or notificaitons or settings, they might receive notifications to their accounts. 
+
+If I can get a reciprocal view rate of > 0.03% (a typical programmatic marketing CTR benchmark) I will consider this to be a success.
+
+(If I have results they'll be in ./results.ipynb)
 
 ### The Story
 
-When I signed up for LinkedIn after business school I quickly noticed that you get a notification when someone else views your profile. In 2014-15 I was looking for work and noticed that when I viewed a random person's profile they would sometimes look at my profile hours, days or weeks later. At that point I had maybe 50 connections, and at the time, I thought that having that fancy blue [500+]() on my profile would help in the future.
+When I signed up for LinkedIn after business school I quickly noticed that you get a notification when someone else views your profile. This was the root of the whole idea.
 
-The first iteration of this tool was [Galaxy-Screen-Scripts](https://github.com/EricCasey/Galaxy-Screen-Scripts) and was simply a recorded screen script using a program that was typically used at the time to make bots for Farmville. All I had to do was search for something like 'recruiter london', start the script, and it would view 1 profile every 2 seconds (to let the page load). It got me the 500+ then I stopped, in fear of getting banned.
+In 2014-ish I was looking for work and noticed that when I viewed a person's profile they would sometimes look at my profile hours, days or weeks later. At that point I had ~50 connections, and at the time, I thought that having that fancy blue [500+]() on my profile would help in the future. Maybe a recruiter would see [500+]() and think "damn, this guy is connected. my client/boss is going to love this guy" or my enemies from middle school would see it and it would be nice cold revenge (_yikes_, I know...).  
 
-**This time though...** I'm going all in using what I've learned since then.
+Anyway, the first iteration of this tool was [Galaxy-Screen-Scripts](https://github.com/EricCasey/Galaxy-Screen-Scripts) and was simply a thumb-recorded screen script using a program that was typically used at the time to make bots for Farmville. All I had to do was search for something like 'recruiter london', start the script, and it would view 1 profile every 2 seconds (to let the page load). It got me the 500+ then I stopped, in fear of getting banned.
 
-In the field of digital marketing one can
+**This time though...** I'm going all in using what I've learned since then, and doing my best to document it.
+
+(If I have results they'll be in ./results.ipynb)
+
+### See It In Action
+
+<center>
+*gif here
+<img scr=""/>
+</center>
 
 
-### Preview
+### Results
 
+<center>
+* line graph here
+<img scr=""/>
+</center>
 
 ### Contents
 ```python
-./headhunter.py               # Main Scripts
-./results.ipynb               # Analysis
+./README.md                   # You're lookin' at it
+./headhunter.py               # Main bot script
+./results.ipynb               # Post-Mortem
 ./cities.txt                  # Custom list of cities to search in
+./keywords.txt                # Custom list of keywords to search
 ./logs/
-    ./users.txt               # A Log Of All Users Viewed
-    ./views.json              # A Log Of Profile Views
-    ./log-dd-mm-yyyy.json     # A Log Of Other Stats
+    ./master_user_log.txt     # All users viewed so far
+    ./template.json           # DB R\record T\template
+    ./log-dd-mm-yyyy.json     # Log of actions taken by day
 ```
 
 ### Usage
 
+* Download Chromedriver for Selenium
+
+`pip3 install json, time, shutil, signal, random, datetime, selenium`
+
 `~$ python3 ./headhunter.py <linkedin_email> <linkedin_password>`  
 
-Then don't touch that computer or use Linkedin on other devices for a few weeks...
+Then don't touch that computer or use Linkedin on other devices for a few weeks... I ran it on a Raspberry Pi connected to an old TV.
+
+
+
+<!-- 
+# !!! https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
+# https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/country:_C
+# https://www.linkedin.com/search/results/all/?keywords=recruiter&origin=GLOBAL_SEARCH_HEADER
+# https://www.linkedin.com/search/results/people/?facetNetwork=%5B%22O%22%5D&facetProfileLanguage=%5B%22en%22%2C%22fr%22%5D&keywords=recruiter&origin=FACETED_SEARCH
+# https://www.linkedin.com/search/results/people/?facetNetwork=%5B%22O%22%5D&facetProfileLanguage=%5B%22en%22%2C%22fr%22%5D&keywords=recruiter%2C%20acquisition&origin=GLOBAL_SEARCH_HEADER&page=2 -->
 
 
 
--- headhunter.py
+++ headhunter.py
@@ -1,87 +1,134 @@
-import sys
-import json
-import time
-import signal
-import random
-import datetime
-import selenium
+import os, sys, json, time, tqdm, shutil, signal, random
 
+import datetime
 from datetime import datetime, timedelta
 
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-u = "<linkedin_username>"    # username placeholder for debugging
-p = "<linkedin_password>"    # password placeholder for debugging
-
-# print('Number of arguments:', len(sys.argv), 'arguments.')
-# print('Argument List:', str(sys.argv))
-
-# if(len(sys.argv) != 3):
-#     print("Usage: ~$ python3 ./headhunter.py <linkedin_email> <linkedin_password>")
-#     exit()
-# else:
-#     u = list(sys.argv)[1]
-#     p = list(sys.argv)[2]
-
-print(u,p)
-
+####################################################################
+# pls don't use this to bother anyone.
+####################################################################
+if(len(sys.argv) != 3):
+    print("Usage: ~$ python3 ./headhunter.py <linkedin_email> <linkedin_password>")
+    exit()
+else:
+    u = list(sys.argv)[1]
+    p = list(sys.argv)[2]
+#######-####-#####-#################################################
 def keyboardInterruptHandler(signal, frame):
     save()
     exit(0)
-
-browser = webdriver.Chrome(executable_path="./chromedriver")
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
-
+#######-####-#####-#################################################
 def save():
     print("===== ENDING GRACEFULLY ==========")
     end_time = int(round(time.time() * 1000))
 
-    log['stats']['runtime'] = log['stats']['runtime'] + ( end_time - start_time )
+    log['runtime'] = log['runtime'] + ( end_time - start_time )
     log['last_updated'] = str(datetime.now())
 
-    with open('log.json', 'w') as fp:
+    date = datetime.now().strftime("%d-%m-%Y")
+    todays_log = "./logs/log-" + date + ".json"
+
+    with open(todays_log, 'w') as fp:
         json.dump(log, fp)
 
+    with open("./logs/master_user_log.txt", 'w') as fp:
+        for user in master_users:
+            fp.write('%s\n' % user)
+
     browser.quit()
+#######-####-#####-#################################################
+def check_log():
+    # check if the program is using the right log. i.e. it's 12:01
+    print("- Log_Name: " + log['log_name'])
+    current_log = "log-" + datetime.now().strftime("%d-%m-%Y") + ".json"
+    if current_log == log['log_name']:
+        print(log['log_name'])
+        print("Current log is correct")
+    else:
+        print(log['log_name'])
+        print("Current log is wrong")
+#######-####-#####-#################################################
+print("=========== Linkedin Social Engineering Script ============")
+print("      -------'IF YOU VIEW IT THEY WILL COME'-------        ")
+# TODO : gracefully change logs at midnight
 
+print("=---- Downloading & Editing Chromedriver")
 
+# TODO : if not, download it
+# TODO : edit file to make less recognizable to the site
+# https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
+if os.path.isfile("./chromedriver") is False:
+    print("- CHOMEDRIVER IS NOT INSTALLED! Get it here: https://chromedriver.chromium.org/downloads")
+    exit(0)
+else:
+    print("- Chromedriver is installed.")
+#######-####-#####-#################################################
+print("=---- Loading Keywords")
+text_file = open("./keywords.txt", "r").read()
+keywords = text_file.splitlines()
+print("- " + str(len(keywords)) + " Keywords Loaded.")
+#######-####-#####-#################################################
+print("=---- Loading User List")
+text_file = open("./logs/master_user_log.txt", "r").read()
+master_users = text_file.splitlines()
+print("- " + str(len(master_users)) + " Users Loaded.")
+#######-####-#####-#################################################
+print("=---- Loading Log")
+date = datetime.now().strftime("%d-%m-%Y")
+print("=- Today's Date: " + date)
+todays_log = "log-" + date + ".json"
 
-print("=== Linkedin Social Engineering Script ===")
+if os.path.isfile("./" + todays_log) is False:
+    print("=- Creating A New Log For Today.")
+    log_path = "./logs/" + todays_log
+    shutil.copy("./logs/template.json", log_path) 
+else:
+    print("=- Log Exists, Loading it")
 
-print("=---- TODO")
-print("_ Download chromedriver and edit it to avoid detection") # https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
-print("_ ")
+with open("./logs/" + todays_log) as json_file: 
+    log = json.load(json_file) 
 
-print("=---- Loading Chromedriver")
-
-
+log["log_name"] = "log-" + date + ".json"
+#######-####-#####-#################################################
 print("=---- Loading Cities")
 text_file = open("./cities.txt", "r").read()
 cities = text_file.splitlines()
-print("-" + str(len(cities)) + " Cities Loaded")
+print("-" + str(len(cities)) + " Cities Loaded.")
 
-print("=---- Loading Log")
-
-## TODO GET DATE
-
-with open('log.json') as json_file: 
-    log = json.load(json_file) 
 for city in cities:
     try:
-        if(log['cities'][city.replace(" ", "")] == 0):
-            log['cities'][city.replace(" ", "")] = 0
+        if(log['cities'][city.split("#")[1].replace(" ", "")] == 0):
+            log['cities'][city.split("#")[1].replace(" ", "")] = 0
     except KeyError:
-        log['cities'][city.replace(" ", "")] = 0
+        log['cities'][city.split("#")[1].replace(" ", "")] = 0
 
-
-# TODO : update my profile stats
-
-print("- This Week Number: " + str(datetime.now().isocalendar()[1]))
-print("- Views This Week: " + str(log['stats']['views']['2020'][datetime.now().isocalendar()[1] - 1]))
-print("- Total Connections: " + str(log['stats']['connections']['2020'][datetime.now().isocalendar()[1] - 1]))
-print("- Total Profiles Viewed: " + str(len(log['users'])))
-
+#######-####-#####-#################################################
+# Search URL Setup
+profile_languages = '["en","fr"]'   # tea & baguette
+connection_degree = '["O"]'         # 3rd deg connections only
+search_slug = 'https://www.linkedin.com/search/results/people/?origin=GLOBAL_SEARCH_HEADER&facetNetwork=' + connection_degree + '&facetProfileLanguage=' + profile_languages + '&'
+#######-####-#####-#################################################
+def hol_up(x):    # wait a x
+    time.sleep(x) # act normal
+#######-####-#####-#################################################
+def links_2_users(links):
+    # Takes a list of URLs and strips the usernames.
+    usernames = []
+    for link in links:
+        if "https://www.linkedin.com/in/" in link.get_attribute("href"):
+            username = link.get_attribute("href").replace("https://www.linkedin.com/in/", "").replace("/", "")
+            if username not in usernames:
+                usernames.append(username)
+    print("- " + str(len(usernames)) + " Usernames extracted.")
+    return usernames
+#######-####-#####-#################################################
+def add_query(city_id, keyword):
+    if city_id + "-" + keyword not in log['queries']:
+        log['queries'].append(city_id + "-" + keyword)
+#######-####-#####-#################################################
 def login():
     browser.get("https://www.linkedin.com/login")
     in_user = browser.find_element_by_id("username")
@@ -89,156 +136,78 @@ def login():
     in_pass = browser.find_element_by_id("password")
     in_pass.send_keys(p)
     in_pass.send_keys(Keys.ENTER)
+#######-####-#####-#################################################
+def heck():
+    for city in cities:
+        city_id = city.split("#")[1].replace(" ", "")
+        city_slug = search_slug + '&facetGeoUrn=%5b"' + city_id + '"%5D'
 
-filt_keywords = ['recruiter', 'hiring', 'headhunter', 'human resources', 'acquisition', 'personnel', 'talent', 'placement']
-filt_language = '["en","fr"]'
-filt_connections = '["O"]'
-filt_location = ''
-filt_page = 1
+        for keyword in keywords:
 
-search_slug = 'https://www.linkedin.com/search/results/people/?origin=GLOBAL_SEARCH_HEADER&facetNetwork=' + filt_connections + '&facetProfileLanguage=' + filt_language + '&'
+            check_log()
+            add_query(city_id, keyword)
 
-# print("- Base Search Slug: " + search_slug)
+            browser.get(city_slug)
 
-def get_stats():
-    print("--- get stats in this func")
-
-def kill_session():
-    browser.close()
-
-def hol_up():
-    time.sleep(3)
-
-def links_2_users(links):
-    usernames = []
-    for link in links:
-    # print(link.get_attribute("href"))
-        if "https://www.linkedin.com/in/" in link.get_attribute("href"):
-            # print(link.get_attribute("href"))
-            username = link.get_attribute("href").replace("https://www.linkedin.com/in/", "").replace("/", "")
-            if username not in usernames:
-                usernames.append(username)
-
-    print("- " + str(len(usernames)) + " usernames extracted")
-
-    return usernames
-
-def begin():
-    print("=---- STARTING SEARCHES")
-
-    # LOOP CITIES
-    #   LOOP KEYWORDS
-
-    # for city in cities:
-# try: 
-
-    for city in [ "San Diego, California", "Tokyo, Japan" ]:
-
-        print("=-- Searching in  + city")
-
-        # for job_title in list_of_hr-related_job_titles
-        for keyword in filt_keywords:
-
-            browser.get(search_slug)
-
-            print('=- "' + keyword + '"')
-
-            hol_up()
-            # browser.find_element_by_id('ember286').click()                                # Close Chat
-            browser.find_element_by_xpath('/html/body/div[7]/aside/div[1]/header/section[2]/button[2]').click()
-            browser.execute_script("document.getElementById('msg-overlay').style.display = 'none';")
-            hol_up()
-            # browser.find_element_by_id('ember180').click()                                # Click Location Dropdown
-            browser.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[4]/form/button').click()
-            hol_up()
-            # browser.find_element_by_xpath('//*[@id="ember185"]/input').send_keys(city)    # Input City
-            browser.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[4]/form/div/fieldset/div/div[1]/div/div/input').send_keys(city)
-            hol_up()
-            # browser.find_element_by_id('triggered-expanded-ember184').click()             # Click Dropdown Result
-            browser.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[4]/form/div/fieldset/div/div[1]/div/div[2]').click()
-            hol_up()
-            # browser.find_element_by_id('ember189').click()                                # Click Apply
-            browser.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[4]/form/div/fieldset/div/div[2]/div/button[2]/span').click()
-            hol_up()
-            # browser.find_element_by_xpath('//*[@id="ember16"]/input').send_keys("test")   # Input Keyword To Search 
-            browser.find_element_by_xpath('/html/body/div[7]/header/div[2]/div/div/div[1]/div/input').send_keys(keyword)
-            browser.find_element_by_class_name('search-global-typeahead__button').click() # Click search button
-            hol_up()
+            hol_up(3)
+            browser.execute_script("document.getElementById('msg-overlay').style.display = 'none';")                       # Close Chat
+            browser.find_element_by_xpath('/html/body/div[7]/header/div[2]/div/div/div[1]/div/input').send_keys(keyword)   # Input keyword
+            browser.find_element_by_class_name('search-global-typeahead__button').click()                                  # Click search button
+            hol_up(5)
             
             res_count = browser.find_element_by_class_name('search-results__total').text.split(" ")[1]
             page_count = 100
             if int(res_count.replace(",","")) < 100: 
                 page_count = res_count / 10
 
-            print("- " + str(res_count) + " Results For '" + keyword + "' in '" + city + "'.")
+            print("- " + str(res_count) + " Results For '" + keyword + "' in '" + city.split("#")[0].replace(" ", "") + "'.")
             print("- " + str(page_count) + " Pages of Results")
             
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            hol_up()
+
+            hol_up(3)
 
             query_url = browser.current_url
             batch = []
     
             print("===== Collecting Usernames From Query")
             print("=--- Page 1 ----")
+
             links = browser.find_elements_by_xpath("//a[@href]")
             users = links_2_users(links)
             batch.extend(users)
-            print(users)
+            print("- " + str(len(batch)) + " Users in this batch so far.")
 
-            # TMP
-            page_count = 3
-            # TMP
-
-            for page in range(2, page_count):
+            for page in range(2, 3):  # page_count
                 print("=--- Page " + str(page) + " ----")
                 page_url = query_url + "&page=" + str(page)
-                hol_up()
                 browser.get(page_url)
-                hol_up()
+                hol_up(3)
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 links = browser.find_elements_by_xpath("//a[@href]")
                 users = links_2_users(links)
-                hol_up()
                 batch.extend(users)
+                hol_up(2)
             
-            print("===== Viewing Each Profile ")
-            print("- " + str(len(batch)) + " Usernames Connected in this batch")
+            # print("===== Viewing Each Profile In This Batch")
+            # print("- " + str(len(batch)) + " Usernames Connected in this batch")
         
             for usr in batch:
                 
-                if usr not in log['users']:
-                    hol_up()
+                if usr not in log['users'] and usr not in master_users:
+                    hol_up(3)
                     browser.get("https://www.linkedin.com/in/" + usr + "/")
                     log['users'].append(usr)
-                    log['cities'][city.replace(" ", "")] += 1
-                    hol_up()
+                    master_users.append(usr)
+                    log['cities'][city.split("#")[1].replace(" ", "")] += 1
+                    hol_up(3)   
+#######-####-#####-#################################################
+print('=========== STARTING HECK =================================')
 
-            #################################
-
-
-            # time.sleep(30)
-
-            # kill_session()
-
-# except(err):
-#     print(err)
-#     save()      
-
-print('=== MAIN ===')
 start_time = int(round(time.time() * 1000))
-# login()
-# get_stats()
-# begin()
-
-
-
-
-# !!! https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
-
-# https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/country:_C
-
-# https://www.linkedin.com/search/results/all/?keywords=recruiter&origin=GLOBAL_SEARCH_HEADER
-# https://www.linkedin.com/search/results/people/?facetNetwork=%5B%22O%22%5D&facetProfileLanguage=%5B%22en%22%2C%22fr%22%5D&keywords=recruiter&origin=FACETED_SEARCH
-# https://www.linkedin.com/search/results/people/?facetNetwork=%5B%22O%22%5D&facetProfileLanguage=%5B%22en%22%2C%22fr%22%5D&keywords=recruiter%2C%20acquisition&origin=GLOBAL_SEARCH_HEADER&page=2
-
-
+browser = webdriver.Chrome(executable_path="./chromedriver")
+login()
+try:
+    heck()
+except:
+    save()
